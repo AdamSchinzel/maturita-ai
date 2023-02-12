a99c8179e878f475d18ad1b17bc33b2bc31f29b7
AdamSchinzel <schinzel.adam@gmail.com>
2023-02-12 03:23:12 +0100

    feat: add seminar work

---
+++ src/pages/api/seminar-work.ts
@@ -0,0 +1,35 @@
+const generateSeminarWork = async ({ about, language, type }: any) => {
+  try {
+    const response = await fetch("https://api.openai.com/v1/engines/text-davinci-003/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        prompt: `Napiš ${type} na téma ${about} v jazyce ${language} v rozsahu minimálně 300 slov (zobraz v HTML kódu a místo /n dávej <br>), text rozděl do odstavců a nahoru napiš tučným písmem téma práce. Na konci práce udelěj dva prádzné řádky a napiš co je pro daný slohový útvar typické.`,
+        max_tokens: 2000,
+        temperature: 0.7,
+      }),
+    });
+    const data = await response.json();
+
+    return data.choices[0].text;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export default async function handler(req: any, res: any) {
+  const { about, language, type } = req.body;
+
+  const seminarWork = await generateSeminarWork({
+    about,
+    language,
+    type,
+  });
+
+  res.status(200).json({
+    seminarWork,
+  });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import {
   Flex,
   Heading,
   Input,
+  Select,
   Spinner,
   Tab,
   TabList,
@@ -27,10 +28,17 @@ const Home = () => {
   const [title, setTitle] = useState<string>("");
   const [autor, setAutor] = useState<string>("");
   const [question, setQuestion] = useState<string>("");
+  const [about, setAbout] = useState<string>("");
+  const [language, setLanguage] = useState<string>("");
+  const [type, setType] = useState<string>("");
+
   const [loadingBook, setLoadingBook] = useState<boolean>(false);
   const [loadingQuestion, setLoadingQuestion] = useState<boolean>(false);
+  const [loadingSeminarWork, setLoadingSeminarWork] = useState<boolean>(false);
+
   const [bookDescription, setbookDescription] = useState<string>("");
   const [questionDescription, setQuestionDescription] = useState<string>("");
+  const [seminarWork, setSeminarWork] = useState<string>("");
 
   const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
 
@@ -100,6 +108,41 @@ const Home = () => {
     setLoadingQuestion(false);
   };
 
+  const fetchSeminarWork = async () => {
+    setLoadingSeminarWork(true);
+
+    try {
+      const response = await fetch("/api/seminar-work", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          about,
+          language,
+          type,
+        }),
+      });
+      const data = await response.json();
+
+      setSeminarWork(data?.seminarWork);
+    } catch (err) {
+      console.log("error: ", err);
+      toast({
+        title: "Stala se chyba",
+        description: "Umělá intelegence neposkytla odpoveď.",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+
+    setAbout("");
+    setLanguage("");
+    setType("");
+    setLoadingSeminarWork(false);
+  };
+
   return (
     <Flex direction="column" alignItems="center" height="100vh">
       <Heading size="xl" pt={16} pb={3} px={3}>
@@ -107,7 +150,7 @@ const Home = () => {
       </Heading>
       <Flex alignItems="center" px={3}>
         <Text fontSize="lg" textAlign="center" mr={2}>
-          Umělá inteligence pro vypracování knih nebo otázek k maturitě.
+          Umělá inteligence pro vypracování knih, otázek k maturitě nebo napsání písemné práce.
         </Text>
         <Tooltip label="Otázky vypracovává OpenAI" placement="bottom">
           <InfoOutlineIcon />
@@ -117,6 +160,7 @@ const Home = () => {
         <TabList>
           <Tab>Knihy</Tab>
           <Tab>Otázky</Tab>
+          <Tab>Seminární práce</Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
@@ -211,6 +255,69 @@ const Home = () => {
                     Výsledek
                   </Text>
                   <div style={{ marginTop: "10px" }} dangerouslySetInnerHTML={{ __html: questionDescription }}></div>
+                </>
+              )
+            )}
+          </TabPanel>
+          <TabPanel>
+            <Flex flexDir={["column"]}>
+              <Input
+                value={about}
+                onChange={(e) => setAbout(e.target.value)}
+                placeholder="Téma seminární práce"
+                mb={3}
+                isDisabled={loadingSeminarWork}
+                colorScheme="cyan"
+              />
+              <Select
+                onChange={(e) => setLanguage(e.target.value)}
+                value={language}
+                colorScheme="cyan"
+                mb={3}
+                isDisabled={loadingSeminarWork}
+                placeholder="Vyberte jazyk">
+                <option value="Čeština">Čestina</option>
+                <option value="Angličtina">Angličtina</option>
+                <option value="Španělština">Španělština</option>
+                <option value="Francouzština">Francouzština</option>
+                <option value="Ruština">Ruština</option>
+              </Select>
+              <Select
+                onChange={(e) => setType(e.target.value)}
+                value={type}
+                colorScheme="cyan"
+                mb={5}
+                isDisabled={loadingSeminarWork}
+                placeholder="Vyberte slohový útvar">
+                <option value="vyprávění">Vyprávění</option>
+                <option value="formální dopis">Formální dopis</option>
+                <option value="úvaha">Úvaha</option>
+                <option value="reportáž">Reportáž</option>
+              </Select>
+              <Button
+                variant="solid"
+                colorScheme="cyan"
+                onClick={fetchSeminarWork}
+                isDisabled={loadingSeminarWork || !about || !language || !type}>
+                Napsat práci
+              </Button>
+            </Flex>
+            {loadingSeminarWork ? (
+              <>
+                <Flex justifyContent="center">
+                  <Spinner size="lg" color="cyan" mt={20} />
+                </Flex>
+                <Text textAlign="center" mt={5}>
+                  Tato operace může nějakou dobu trvat...
+                </Text>
+              </>
+            ) : (
+              seminarWork && (
+                <>
+                  <Text fontSize="2xl" fontWeight="bold" mt={10}>
+                    Výsledek
+                  </Text>
+                  <div style={{ marginTop: "10px" }} dangerouslySetInnerHTML={{ __html: seminarWork }}></div>
                 </>
               )
             )}
